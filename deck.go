8b5f8c93c1a5494c00e89d0900e617fd420606c8
brijesh <brijesh.nimap@grab.in>
2022-11-24 12:34:15 +0530

    deck created

--- deck.go
+++ deck.go
@@ -1,3 +1,26 @@
 package main
 
+import "fmt"
+
 type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuites := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuites {
+		for _, values := range cardValues {
+			cards = append(cards, values+" of "+suit)
+		}
+
+	}
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
--- main.go
+++ main.go
@@ -1,17 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := deck{"Five of Diamonds", newCard()}
-	// fmt.Println(card)
-	cards = append(cards, "six of Spades")
-	for i, card := range cards {
-		fmt.Println(i, card)
-
-	}
-}
-
-func newCard() string {
-	return "Ace of Spades"
+	cards := newDeck()
+	cards.print()
 }
